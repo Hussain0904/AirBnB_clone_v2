0a850c5719f41c22cc421af44fdc10f77f167f9f
delayer2208 <Veresan72@gmail.com>
2024-06-04 21:52:19 +0200

    Finish

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,4 +12,17 @@ app = Flask(__name__)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
     """Render a template with states and their cities."""
-    states
+    states = storage.all("State")
+    if state_id:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the storage on teardown."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
